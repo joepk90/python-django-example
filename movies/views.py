b8a8fd17396007f578e872f2e3ed2bb6c2527d13
Joe <jparkkennaby@gmail.com>
2020-12-04 14:56:40 +0000

    10 - 11 - setup movie index template. update view to use template. render movies table

--- movies/views.py
+++ movies/views.py
@@ -7,15 +7,12 @@ from . models import Movie
 
 def index(request):
 
-    # SELECT * FROM movies_movie
-    movies = Movie.objects.all()
-
-    output = ','.join([m.title for m in movies])
-
     # SELECT * FROM movies_movie WHERE released_year=1984
     # Movie.objects.fitler(released_year=1984)
 
     # SELECT * FROM movies_movie WHERE id=1
     # Movie.objects.get(id=1)
 
-    return HttpResponse(output)
+    # SELECT * FROM movies_movie
+    movies = Movie.objects.all()
+    return render(request, "movies/index.html", {'movies': movies})
