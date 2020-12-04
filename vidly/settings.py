1ac569849d97189fe04e71b19ffc1915228e26a9
Joe <jparkkennaby@gmail.com>
2020-12-04 15:18:24 +0000

    10 - 14 - setup project root template directory. move movies base.html file to the project root directory. update movies index.html to use the base.html template from the root template directory

--- vidly/settings.py
+++ vidly/settings.py
@@ -55,7 +55,8 @@ ROOT_URLCONF = 'vidly.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        # adds template directory in project root
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
