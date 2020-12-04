27717e5f98a4e426e63ff8c0ef73a0282c3755c6
Joe <jparkkennaby@gmail.com>
2020-12-04 13:49:35 +0000

    10 - 10 - setup movie index view to query movie objects. add extra query documentation

--- movies/views.py
+++ movies/views.py
@@ -1,8 +1,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from . models import Movie
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("hello world")
+
+    # SELECT * FROM movies_movie
+    movies = Movie.objects.all()
+
+    output = ','.join([m.title for m in movies])
+
+    # SELECT * FROM movies_movie WHERE released_year=1984
+    # Movie.objects.fitler(released_year=1984)
+
+    # SELECT * FROM movies_movie WHERE id=1
+    # Movie.objects.get(id=1)
+
+    return HttpResponse(output)
