7a7ef223337ae1a70bf6d058a6f1245b4fc04059
Joe <jparkkennaby@gmail.com>
2020-12-04 12:00:31 +0000

    10 - 05 - setup movie models: Movie, Genre

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    released_year = models.IntegerField()
+    number_in_stock = models.IntegerField()
+    daily_rate = models.FloatField()
+
+    # CASCADING: delete all movies related to genre, if genre is deleted
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
